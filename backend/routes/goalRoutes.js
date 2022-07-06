c2096f091951a9b442301e5903608fd5c0a7e5a1
brianmujjuni <brianmujjuni@engineer.com>
2022-07-06 08:34:18 -0700

    Initial rest api for goal

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,7 +6,7 @@ router.route('/').get(getGoals).post(setGoal)
 //router.get('/', getGoals)
 //router.post('/', setGoal)
 
-router.route('/:id').delete(updateGoal).put(deleteGoal)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 //router.put('/:id', updateGoal)
 //router.delete('/:id', deleteGoal)
 
