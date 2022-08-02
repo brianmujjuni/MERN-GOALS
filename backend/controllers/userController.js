1ce100597575e38ee27ee6a3445d80453e026c59
brianmujjuni <brianmujjuni@engineer.com>
2022-08-02 14:08:44 -0700

    edited controllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,12 +70,7 @@ const loginUser = asyncHandler(async (req, res) => {
 // get user data
 // private route
 const getMe = asyncHandler(async (req, res) =>{
-    const {_id ,name ,email} = await User.findById(req.user.id)
-    res.status(200).json({
-        id: _id,
-        name,
-        email
-    })
+    res.status.apply(200).json(req.user)
     
 })
 
